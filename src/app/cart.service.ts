004668c11bdb58b7f8aa2b74f358288bfab58666
manu020766 <manu020766@yahoo.es>
2019-11-25 12:18:39 +0100

    add form in cart component

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   items=[];
+
+  constructor(private http: HttpClient ) {}
 
   addItem(producto) {
     return this.items.push(producto);
@@ -15,5 +18,8 @@ export class CartService {
   clearCart() {
     this.items=[]
     return this.items;
+  }
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
   }
 }
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { CartService } from '../cart.service' 
+import { FormBuilder } from '@angular/forms' 
+
+import { CartService } from '../cart.service';
+
 
 @Component({
   selector: 'app-cart',
@@ -7,12 +10,25 @@ import { CartService } from '../cart.service'
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  cartItems = []
+  cartItems;
+  checkoutForm;
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.cartItems = this.cartService.getItems()
+
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      adress: ''
+    })
+  }
+
+  onSubmit(customerData) {
+    console.log('Your order has been submitted', customerData)
+
+    this.cartService.clearCart()
+    this.checkoutForm.reset()
   }
 
 }
