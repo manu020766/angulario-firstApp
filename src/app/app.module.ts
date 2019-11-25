29e9d4d188a6610168f8b4e85228e390337630a0
manu020766 <manu020766@yahoo.es>
2019-11-25 12:18:13 +0100

    add shipping component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
@@ -13,6 +15,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 registerLocaleData(localeES);
 
@@ -23,7 +26,8 @@ registerLocaleData(localeES);
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
-    CartComponent
+    CartComponent,
+    ShippingComponent
   ],
   imports: [
     BrowserModule,
@@ -31,9 +35,12 @@ registerLocaleData(localeES);
       {path: '', component: ProductListComponent},
       {path: 'product-details/:id', component: ProductDetailsComponent},
       {path: 'cart', component: CartComponent},
+      {path: 'shipping', component: ShippingComponent},
       {path: '**', redirectTo: ''}
     ]),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [{ provide: LOCALE_ID, useValue: 'es' }],
   bootstrap: [AppComponent]
